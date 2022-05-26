774a804e15db344263a2c84226f777c5ace5fae7
Mike Spertus <msspertu@amazon.com>
2022-05-26 09:50:32 -0500

    Turn fullsimple into dune project. No semantic changes

---
+++ fullsimple/test.f
@@ -0,0 +1,32 @@
+/* Examples for testing */
+
+ "hello";
+
+ 
+lambda x:<a:Bool,b:Bool>. x;
+
+
+unit;
+
+timesfloat 2.0 3.14159;
+
+let x=true in x;
+
+{x=true, y=false}; 
+{x=true, y=false}.x;
+{true, false}; 
+{true, false}.1; 
+
+
+lambda x:Bool. x;
+(lambda x:Bool->Bool. if x false then true else false) 
+  (lambda x:Bool. if x then false else true); 
+
+lambda x:Nat. succ x;
+(lambda x:Nat. succ (succ x)) (succ 0); 
+
+T = Nat->Nat;
+lambda f:T. lambda x:Nat. f (f x);
+
+
+lambda x:A. x;
