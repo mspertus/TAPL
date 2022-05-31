1f80baa6f12f6936ebdc2c1da048bd43f41d04b9
Mike Spertus <msspertu@amazon.com>
2022-05-31 10:46:36 -0500

    Add more example from section 13.1

--- fullref/chapter_13.f
+++ fullref/chapter_13.f
@@ -15,3 +15,40 @@ r := 7;
 s = r;
 s := 82;
 !r;
+
+/* 13.1 Shared State */
+c = ref 0;
+
+incc = lambda x: Unit. (c := succ (!c); !c);
+decc = lambda x: Unit. (c := pred (!c); !c);
+
+incc unit;
+decc unit;
+
+o = {i = incc, d = decc};
+
+/* 13.1 References to Compound Types */
+NatArray = Ref (Nat->Nat);
+newarray = lambda _:Unit. ref (lambda n:Nat.0);
+lookup = lambda a:NatArray. lambda n:Nat. (!a) n;
+
+/* I had to add this for update. Note that this equal,
+   which works on Nats, differs from the one in fulluntyped,
+   which works on Church numerals                           */
+eql = lambda eq:Nat -> Nat -> Bool. lambda m:Nat. lambda n:Nat.
+    if iszero m then iszero n
+    else if iszero n then false
+    else eq (pred m) (pred n);
+
+
+equal = fix eql;
+
+update = lambda a:NatArray. lambda m:Nat. lambda v:Nat.
+    let oldf = !a in
+    a := (lambda n:Nat. if equal m n then v else oldf n);
+
+arr = newarray unit;
+update arr 3 5;
+arr 2;
+arr 3;
+
