04907db710a2550a57b9b19491e976f42b1e21b5
Mike Spertus <msspertu@amazon.com>
2022-06-02 10:51:15 -0500

    Convert rcdsub into dune project. No semantic changes
    Note: This implementation is named rcdsub in the textbook
    but rcdsubbot in the implementations supplied on the book website.
    I chose the name rcdsub in accordance with the note at the start
    of chapter 15 of the textbook.

---
+++ rcdsub/lib/core.mli
@@ -0,0 +1,11 @@
+(* module Core
+
+   Core typechecking and evaluation functions
+*)
+
+open Syntax
+open Support.Error
+
+val eval : context -> term -> term 
+val typeof : context -> term -> ty
+val subtype : ty -> ty -> bool
---
+++ rcdsub/lib/syntax.mli
@@ -0,0 +1,53 @@
+(* module Syntax: syntax trees and associated support functions *)
+
+open Support.Pervasive
+open Support.Error
+
+(* Data type definitions *)
+type ty =
+    TyTop
+  | TyBot
+  | TyArr of ty * ty
+  | TyRecord of (string * ty) list
+
+type term =
+    TmRecord of info * (string * term) list
+  | TmProj of info * term * string
+  | TmVar of info * int * int
+  | TmAbs of info * string * ty * term
+  | TmApp of info * term * term
+
+type binding =
+    NameBind 
+  | VarBind of ty
+
+type command =
+    Import of string
+  | Eval of info * term
+  | Bind of info * string * binding
+
+(* Contexts *)
+type context
+val emptycontext : context 
+val ctxlength : context -> int
+val addbinding : context -> string -> binding -> context
+val addname: context -> string -> context
+val index2name : info -> context -> int -> string
+val getbinding : info -> context -> int -> binding
+val name2index : info -> context -> string -> int
+val isnamebound : context -> string -> bool
+val getTypeFromContext : info -> context -> int -> ty
+
+(* Shifting and substitution *)
+val termShift: int -> term -> term
+val termSubstTop: term -> term -> term
+
+(* Printing *)
+val printtm: context -> term -> unit
+val printtm_ATerm: bool -> context -> term -> unit
+val printty : ty -> unit
+val prbinding : context -> binding -> unit
+
+(* Misc *)
+val tmInfo: term -> info
+
